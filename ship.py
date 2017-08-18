652dad99199ae3715619d02aa70a1f2fc9137c12
link375 <https://github.com/link375>
2017-08-17 22:22:02 -0600

    Made the ship move slightly faster

--- ship.py
+++ ship.py
@@ -4,9 +4,10 @@ import pygame
 class Ship():
 
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set it's starting position"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # load the ship image and get it's rect
         self.image = pygame.image.load("images/etherum.png")
@@ -20,16 +21,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Store a decimal value for the ship's center
+        self.center = float(self.rect.centerx)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on the movement flags"""
+        # Update the ship's center value, not the rect
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -=1
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+            self.rect.centerx = self.center
 
 
     def blitme(self):
