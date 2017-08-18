05eacbcffa9d56fe7c5551f1db139a6dddaa79cc
link375 <https://github.com/link375>
2017-08-17 21:32:13 -0600

    Added a game functions file to clean up code

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,7 @@
-import sys
 import pygame
 from settings import Settings
 from ship import Ship
-
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings and create a screen object.
@@ -18,11 +17,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-
-        # watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        #check events from the game functions file
+        gf.check_events()
 
         # Redraw the screen during each pass through the loop
         # add the background color
