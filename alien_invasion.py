ee41e1776b76fd6f12022f291bd874b7879d50f1
link375 <https://github.com/link375>
2017-09-24 14:02:20 -0600

    added updates to all items in the game loop

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -18,11 +19,15 @@ def run_game():
     # make a ship
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
         # check events from the game functions file
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
